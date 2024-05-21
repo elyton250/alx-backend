35fa9e94554d58af33f54170ab0aef60b094d6b7
Elyse <k.rwihimba@alustudent.com>
2024-05-21 11:45:25 +0200

    task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,49 +1,48 @@
 #!/usr/bin/env python3
-"""This paginates a database"""
+"""
+Adds `get_page` method to `Server` class
+"""
 import csv
-from typing import List, Tuple, Optional
+from typing import List, Tuple
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
-    DATA_FILE: str = "Popular_Baby_Names.csv"
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
 
-    def __init__(self) -> None:
-        self.__dataset: Optional[List[List[str]]] = None
+    def __init__(self):
+        self.__dataset = None
 
-    def dataset(self) -> List[List[str]]:
-        """Cached dataset"""
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
-                dataset: List[List[str]] = [row for row in reader]
+                dataset = [row for row in reader]
             self.__dataset = dataset[1:]
+
         return self.__dataset
 
     @staticmethod
     def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """Returns a tuple of the first and the last index"""
-        try:
-            assert page > 0
-            assert page_size > 0
-        except AssertionError as e:
-            print('please input a positive integer as page or page_size')
-        start_index: int = (page - 1) * page_size
-        end_index: int = start_index + page_size
-        return (start_index, end_index)
+        """Calculate start and end index range for a `page`, with `page_size`
+        """
+        nextPageStartIndex = page * page_size
+        return nextPageStartIndex - page_size, nextPageStartIndex
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> Optional[List[List[str]]]:
-        """This paginates the dataset"""
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-        
-        if self.__dataset is None:
-            print("Dataset is None. Please initialize the dataset.")
-            return None
-        else:
-            total_pages: float = len(self.__dataset) / page_size
-            if page > total_pages:
-                return []
-            else:
-                indexes: Tuple[int, int] = self.index_range(page, page_size)
-                return self.__dataset[indexes[0]:indexes[1]]
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Get items for the given page number
+        Args:
+            page (int): page number
+            page_size (int): number of items per page
+        Returns:
+            (List[List]): a list of list(row) if inputs are within range
+            ([]) : an empty list if page and page_size are out of range
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        startIndex, endIndex = self.index_range(page, page_size)
+        return self.dataset()[startIndex:endIndex]
