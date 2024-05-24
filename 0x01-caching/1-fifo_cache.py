89ba1e7b494e1a15bc9de3493ffa80fea7edf6a6
elyse <nelysemathieu@gmail.com@>
2024-05-24 12:26:51 +0200

    task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""This is a new caching system"""
+from collections import deque
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """This is the caching system"""
+    def __init__(self):
+        """This initializes the class"""
+        super().__init__()
+        self.keys = deque()
+
+    def put(self, key, item):
+        """This is the put method"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.keys.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = self.keys.popleft()
+                self.cache_data.pop(discarded_key)
+                print(f'DISCARD: {discarded_key}')
+            self.cache_data[key] = item
+            self.keys.append(key)
+
+    def get(self, key):
+        """This gets the key"""
+        return self.cache_data.get(key, None)
