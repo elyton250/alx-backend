59ee0adb16b56e7a04278b819a36aabdf33566eb
elyse <nelysemathieu@gmail.com@>
2024-05-28 22:59:26 +0200

    tasks

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -2,7 +2,8 @@
 """
 A Basic flask application
 """
-from flask import Flask, request
+from flask import Flask
+from flask import request
 from flask import render_template
 from flask_babel import Babel
 
@@ -25,8 +26,10 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
-    """this function gets locale"""
+def get_locale() -> str:
+    """
+    Gets locale from request object
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
@@ -35,7 +38,7 @@ def index() -> str:
     """
     Renders a basic html template
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
